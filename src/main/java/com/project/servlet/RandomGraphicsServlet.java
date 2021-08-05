074f4a2af418bd22db56a2b864e30ee577290171
YaroslavChirko <yarikchirko@gmail.com>
2021-08-05 17:31:27 +0300

    started added servlet code, implementing jsp logic

--- src/main/java/com/project/servlet/RandomGraphicsServlet.java
+++ src/main/java/com/project/servlet/RandomGraphicsServlet.java
@@ -2,24 +2,48 @@ package com.project.servlet;
 
 import java.io.IOException;
 
+import com.project.randomgraphics.RandomCubeProvider;
+import com.project.randomgraphics.RandomShapeProviderInterface;
+import com.project.randomgraphics.elements.Graphic;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet
+@WebServlet("/random")
 public class RandomGraphicsServlet extends HttpServlet {
 	private static final long serialVersionUID = 238731237398195710L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+		//get shape generator
+		RandomShapeProviderInterface provider = new RandomCubeProvider(200,200,10);
+		//call function to generate the shapes
+		Graphic [] graphics = provider.getGraphic();
+		
+		req.setAttribute("graphics", graphics);
+		
+		this.doPost(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+		Graphic[] graphics;
+		//check for the argument
+		//add cast check later
+		 graphics = (Graphic[]) req.getAttribute("graphics");
+		if(graphics == null) {
+			//if arg is not present call the function
+			RandomShapeProviderInterface provider = new RandomCubeProvider(200,200,10);
+			graphics = provider.getGraphic();
+		}
+		
+		//add argument to the body
+		req.setAttribute("graphics", graphics);
+		//return jsp page
+		req.getRequestDispatcher("random-graphics.jsp").include(req,resp);
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
+<!DOCTYPE html>
 <html>
 <body>
 <h2>Hello World!</h2>
+<br><br><br>
+<a href="/random"></a>
 </body>
 </html>
